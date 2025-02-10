b53ceacdaa7c923b46194c9e81502056c529261c
LDCaster <devoners9@example.com>
2025-02-10 10:16:05 +0800

    Update Pertama

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,41 @@
+<aside class="sidebar-wrapper" data-simplebar="true">
+    <div class="sidebar-header">
+      <div>
+        <img src="assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+      </div>
+      <div>
+        <h4 class="logo-text">SISIP</h4>
+      </div>
+      <div class="toggle-icon ms-auto"> <i class="bi bi-list"></i>
+      </div>
+    </div>
+    <!--navigation-->
+    <ul class="metismenu" id="menu">
+      <li>
+        <a href="javascript:;">
+          <div class="parent-icon"><i class="bi bi-house-fill"></i>
+          </div>
+          <div class="menu-title">Dashboard</div>
+        </a>
+      </li>
+
+      <li class="menu-label">Menu</li>
+      <li>
+        <a href="pages-user-profile.html">
+          <div class="parent-icon"><i class="bi bi-file-earmark-break-fill"></i>
+          </div>
+          <div class="menu-title">Data Soal</div>
+        </a>
+      </li>
+
+      <li class="menu-label">Data Master</li>
+      <li>
+        <a href="pages-user-profile.html">
+          <div class="parent-icon"><i class="bi bi-person-lines-fill"></i>
+          </div>
+          <div class="menu-title">Users</div>
+        </a>
+      </li>
+    </ul>
+    <!--end navigation-->
+ </aside>
